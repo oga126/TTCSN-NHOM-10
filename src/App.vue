49957eaba7f78fc492cb16511b6817724bd5e6c1
oga123 <spurini01@gmail.com>
2023-04-21 17:44:23 +0700

    setup

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import defaultLayout from './layouts/default-layout.vue';
-</script>
-
-<template>
-  <default-layout>
-    <router-view></router-view>
-  </default-layout>
-</template>
--- src/constants/index.js
+++
@@ -1,6 +0,0 @@
-const TEL = {
-  href: 'tel:0982888431',
-  display: '0982.888.431'
-};
-
-export { TEL };
